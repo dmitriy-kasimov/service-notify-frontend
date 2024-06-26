0cb26cbe972fd363e278eb4155434eeca6186aec
Trevor_Phillips <tr271v0r.phillips.industries@gmail.com>
2024-06-26 20:20:31 +0300

    add the feature with modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AlertsPull } from './AlertsPull/AlertsPull.tsx';
 import { NotificationsPull } from './NotificationsPull/NotificationsPull.tsx';
+import { ModalInfo } from './Modals/ModalInfo.tsx';
 
 function App() {
     return (
         <>
             <AlertsPull />
             <NotificationsPull />
+            <ModalInfo />
         </>
     );
 }
