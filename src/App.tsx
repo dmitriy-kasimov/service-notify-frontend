f44a42c9b9c268cca8fc079e657d19ec5cd0d531
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-28 21:22:45 +0300

    removed tested data from <App />

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,11 @@ import { AlertsPull } from './AlertsPull/AlertsPull.tsx';
 import { NotificationsPull } from './NotificationsPull/NotificationsPull.tsx';
 
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify';
-
 
 function App() {
-    const onClick = () => {
-        toast('Wow so easy!', {
-            position: 'top-center',
-            autoClose: 3000,
-        });
-    }
 
     return (
         <>
-            <button onClick={onClick}>TOAST</button>
             <AlertsPull />
             <NotificationsPull />
         </>
